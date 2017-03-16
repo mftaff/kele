1300cb9e99a9347b678fc52ee447532e7e4ce8bc
Meir Taffel <artgardening@gmail.com>
2017-03-16 11:23:25 +0000

    add get_me method: parse's user data into ruby hash. add auth_token: posts email and password to confirm identity

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,29 @@
-class Kele 
-    def initialize(username, password)
-        @username = username
-        @password = password
+require 'httparty'
+require 'json'
+
+class Kele
+    include HTTParty
+    
+    def initialize(email, password)
+        @base_url = 'https://www.bloc.io/api/v1'
+        options = {
+            body: {
+                password: password,
+                email: email
+            }
+        }
+        
+        response = self.class.post("#{@base_url}/sessions", options)
+        @auth_token = response['auth_token']
+        
+        if response.nil? || response['auth_token'].nil?
+            raise ArgumentError.new("The system was unable to authorize you.")
+        end
     end
     
-    def self.hello
-        puts "hello world"
+    def get_me
+        response = self.class.get("#{@base_url}/users/me", headers: { "authorization": @auth_token })
+        # puts ">>> response: #{response.inspect}"
+        JSON.parse(response.body)
     end
 end
